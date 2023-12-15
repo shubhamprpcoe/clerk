103f2111204011a7fe73f7300791de4562db092b
shubhamprpcoe <shubhamprpcoe@gmail.com>
2023-12-16 00:31:09 +0530

    basic code

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("rendering files")
+const express = require('express');
+const app = express();
+const port = 4000; // You can use any available port
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
